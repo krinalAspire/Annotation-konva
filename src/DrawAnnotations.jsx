d2fc3b8d59fcc558fa2b6a1ad3308fbc7d4ebbd2
krinal <krinal.sonara@aspiresoftserv.in>
2024-01-02 13:07:35 +0530

    2 jan 2023, 1:07 pm

--- src/DrawAnnotations.jsx
+++ src/DrawAnnotations.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Image, Layer, Rect, Stage } from "react-konva";
 import Icon from "./invoice_7.jpg";
+import img7 from "./geometric-architecture-project-invoice_23-2149660465.avif";
 import Konva from "konva";
 import randomstring from "randomstring";
 import { initialAnnotations } from "./annotationdata";
@@ -18,13 +19,16 @@ import {
 
 const DrawAnnotations = () => {
   const [annotations, setAnnotations] = useState([]);
+  const [intialData, setintialData] = useState([]);
   const [newAnnotation, setNewAnnotation] = useState([]);
   const [image] = useState(new window.Image());
   const [initialDataColor] = useState(Konva.Util.getRandomColor());
   const [newAnnotationColor] = useState(Konva.Util.getRandomColor());
   const [isDialogOpen, setDialogOpen] = useState(false);
   const [selectedBox, setSelectedBox] = useState(null);
-  const [resizingHandle, setResizingHandle] = useState(null);
+
+  // const originalImageSize = { width: 2481, height: 3508 };
+  const originalImageSize = { width: 1000, height: 1000 };
 
   const randomStr = randomstring.generate(6);
   // console.log("image", image.width);
@@ -35,17 +39,22 @@ const DrawAnnotations = () => {
   useEffect(() => {
     // Replace this with your actual initial data
     setAnnotations(initialAnnotations);
+    setintialData(initialAnnotations);
   }, []);
 
+  const handleMouseEnter = (event) => {
+    event.target.getStage().container().style.cursor = "crosshair";
+  };
+
   const handleMouseDown = (event) => {
-    // console.log('mouse down called');
+    console.log("mouse down called");
     if (newAnnotation.length === 0) {
       const { x, y } = event.target.getStage().getPointerPosition();
       setNewAnnotation([{ x, y, width: 0, height: 0, id: randomStr }]);
     }
   };
 
-  // console.log("annotations", annotations);
+  console.log("annotations", annotations);
   console.log("newAnnotation", newAnnotation);
 
   const handleMouseUp = (event) => {
@@ -114,6 +123,16 @@ const DrawAnnotations = () => {
   const handleCloseDialog = () => {
     setDialogOpen(false);
     setSelectedBox(null);
+    // console.log('selectedbox', selectedBox);
+
+    if (selectedBox !== null) {
+      const updatedAnnotations = annotations.map((box) => {
+        console.log("box", box);
+        return box.id === selectedBox.id ? { ...box, validated: true } : box;
+      });
+      console.log("updatedannotationbox", updatedAnnotations);
+      setAnnotations(updatedAnnotations);
+    }
   };
 
   const handleRemove = () => {
@@ -150,22 +169,40 @@ const DrawAnnotations = () => {
   console.log("dialogbox", isDialogOpen);
 
   const annotationsToDraw = [...annotations, ...newAnnotation];
+
+  const onMouseEnter = (event) => {
+    event.target.getStage().container().style.cursor = "move";
+  };
+
+  const onMouseLeave = (event) => {
+    event.target.getStage().container().style.cursor = "crosshair";
+  };
+
   return (
     <>
       <Grid container>
         <Grid item xs={8.5}>
-          <Box sx={{ width: "100vw", height: "100vh", overflow: "auto" }}>
+          <Box sx={{ width: "70vw", height: "100vh", overflow: "auto" }}>
             <Stage
               onMouseDown={handleMouseDown}
               onMouseUp={handleMouseUp}
               onMouseMove={handleMouseMove}
+              onMouseEnter={handleMouseEnter}
               // width={900}
               // height={700}
-              width={image.width}
-              height={image.height}
+              // width={image.width}
+              // height={image.height}
+              // width={window.innerWidth}
+              // height={window.innerHeight}
+              width={originalImageSize.width}
+              height={originalImageSize.height}
             >
               <Layer>
-                <Image image={image} />
+                <Image
+                  image={image}
+                  scaleX={originalImageSize.width / image.width}
+                  scaleY={originalImageSize.height / image.height}
+                />
               </Layer>
               <Layer>
                 {annotationsToDraw.map((value) => {
@@ -190,17 +227,58 @@ const DrawAnnotations = () => {
                     //   stroke={color}
                     //   draggable
                     // />
+                    // <Rect
+                    //   key={value.id}
+                    //   x={value.x}
+                    //   y={value.y}
+                    //   width={value.width}
+                    //   height={value.height}
+                    //   onMouseEnter={onMouseEnter}
+                    //   onMouseLeave={onMouseLeave}
+                    //   // stroke={color}
+                    //   // stroke={value === newAnnotation[0] ? newAnnotationColor : color}
+                    //   fill="transparent"
+                    //   stroke={value.color || initialDataColor}
+                    //   onClick={() => handleBoxClick(value)}
+                    //   // draggable
+                    // />
                     <Rect
                       key={value.id}
-                      x={value.x}
-                      y={value.y}
-                      width={value.width}
-                      height={value.height}
-                      // stroke={color}
-                      // stroke={value === newAnnotation[0] ? newAnnotationColor : color}
+                      x={
+                        value.label !== undefined
+                          ? value.x * (originalImageSize.width / image.width)
+                          : value.x
+                      }
+                      y={
+                        value.label !== undefined
+                          ? value.y * (originalImageSize.height / image.height)
+                          : value.y
+                      }
+                      width={
+                        value.label !== undefined
+                          ? value.width *
+                            (originalImageSize.width / image.width)
+                          : value.width
+                      }
+                      height={
+                        value.label !== undefined
+                          ? value.height *
+                            (originalImageSize.height / image.height)
+                          : value.height
+                      }
+                      fill="transparent"
                       stroke={value.color || initialDataColor}
                       onClick={() => handleBoxClick(value)}
-                      // draggable
+                      strokeWidth={2} // Adjust the width of the border
+                      dash={value.label === undefined ? [5, 5] : null} // Set a dashed border pattern
+                      // cornerRadius={5} // Set the radius of the corners
+                      cornerRadius={
+                        value.label !== undefined &&
+                        value.width > 0 &&
+                        value.height > 0
+                          ? 5 // Set the radius of the corners for rectangles with positive width and height
+                          : 0 // No corner radius for rectangles with negative width or height
+                      }
                     />
                   );
                 })}
